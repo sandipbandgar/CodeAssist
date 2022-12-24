257c658cfefd14455c37c8ec2809055432b47d74
sandipbandgar <skbandgar2868@gmail.com>
2022-12-24 20:59:12 +0530

    no:-2 commit

--- client/script.js
+++ client/script.js
@@ -75,7 +75,7 @@ const handleSubmit = async (e) => {
 
     loader(messageDiv)
 
-    const response = await fetch('http://localhost:3000', {
+    const response = await fetch('https://codeassist.onrender.com', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
